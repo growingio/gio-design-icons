8ea32659286095a913bf84cb1848a3dd0302d902
Pan Jie <panjie@growingio.com>
2020-12-24 15:54:13 +0800

    chore: initialize repo
    rename some svg files.

---
+++ src/interface.ts
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export interface IconProps {
+  className?: string;
+  style?: React.CSSProperties;
+  rotating?: boolean;
+  color?: string;
+  size?: string;
+  onClick?: (event: React.MouseEvent) => void;
+}
