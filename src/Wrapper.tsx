7706a2e9b3541c17afd3e5e3491c8e3553cbb295
Pan Jie <panjie@growingio.com>
2021-01-17 00:32:14 +0800

    feat: add role and aria-label for icons

--- src/Wrapper.tsx
+++ src/Wrapper.tsx
@@ -4,14 +4,22 @@ import { IconProps } from './interface';
 import './Wrapper.less';
 
 interface WrapperProps extends IconProps {
+  svgName: string;
   icon: React.ReactNode;
 }
 
 const Wrapper: React.FC<WrapperProps> = (props: WrapperProps) => {
-  const { icon, className, ...restProps } = props;
-  const classString = classNames('gio-icon', className);
+  const { icon, className, rotating, svgName, ...restProps } = props;
+  const classString = classNames(
+    'gio-icon',
+    {
+      ['gio-icon-rotating']: rotating,
+    },
+    className
+  );
+  const ariaLabel = svgName.slice(3).replace(/[A-Z]/g, (match) => (match ? `-${match.toLowerCase()}` : '')).slice(1);
   return (
-    <span className={classString} {...restProps}>
+    <span role="img" aria-label={ariaLabel} className={classString} {...restProps}>
       {icon}
     </span>
   );
