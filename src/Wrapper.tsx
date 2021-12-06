bfdea944aae57590c3209ca833912c8e645ee4de
Pan Jie <panjie@growingio.com>
2021-12-06 13:27:05 +0800

    chore(deps): remove the classnames dependency

--- src/Wrapper.tsx
+++ src/Wrapper.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import classNames from 'classnames';
 import type { WrapperProps } from './interfaces';
 
 import './Wrapper.css';
 
 const Wrapper: React.FC<WrapperProps> = (props: WrapperProps) => {
   const { icon, className, rotating, svgName, ...restProps } = props;
-  const classString = classNames(
-    'gio-icon',
-    {
-      ['gio-icon-rotating']: rotating,
-    },
-    className
-  );
+  const classNames = ['gio-icon'];
+  if (rotating) {
+    classNames.push('gio-icon-rotating')
+  }
+  if (className) {
+    classNames.push(className);
+  }
   const ariaLabel = svgName.slice(3).replace(/[A-Z]/g, (match) => (match ? `-${match.toLowerCase()}` : '')).slice(1);
   return (
-    <span role="img" aria-label={ariaLabel} className={classString} {...restProps}>
+    <span role="img" aria-label={ariaLabel} className={classNames.join(' ')} {...restProps}>
       {icon}
     </span>
   );
